5d83d391baa62dbb8b2d5440d331005958178e09
maoya23 <98944921+maoya23@users.noreply.github.com>
2022-02-28 12:10:39 +0900

    Add files via upload

---
+++ MNIST_show.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import sys,os
+sys.path.append(os.pardir)
+from MNIST import load_mnist
+
+(x_train, t_train),(x_test,t_test) = load_mnist(flatten=True,normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
